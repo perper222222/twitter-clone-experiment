4062ece20154dc74089a505be322ea77b4633191
perper222222 <perper222222@gmail.com>
2025-08-08 13:40:30 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@styles/globals.scss';
+import '../styles/globals.scss';
+
 
 import { AuthContextProvider } from '@lib/context/auth-context';
 import { ThemeContextProvider } from '@lib/context/theme-context';
